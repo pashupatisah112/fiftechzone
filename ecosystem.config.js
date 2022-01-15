125f778a006916aad3e5035c29fbd6983672c740
Pashupati <pashupatisah112@gmail.com>
2022-01-15 19:24:56 +0545

    updated ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
     apps: [
       {
         name: 'Fiftechzone',
+        port:'3001',
         exec_mode: 'cluster',
         instances: 'max', // Or a number of instances
         script: './node_modules/nuxt/bin/nuxt.js',
