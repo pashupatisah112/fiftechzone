0348a6a88845fc9922d2c509a3c127aa2299a6d5
pashupati sah <pashupatisah112@gmail.com>
2022-01-23 01:43:00 +0545

    changed default port

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,9 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  server: {
+    port: 8080
+  },
   head: {
     titleTemplate: '%s | Go Digitai Live smart.',
     title: 'Fiftechzone',
