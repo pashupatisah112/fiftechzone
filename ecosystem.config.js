fc19d924b28caa045cc601d19e9d91dc03378087
pashupati sah <pashupatisah112@gmail.com>
2022-01-22 23:47:23 +0545

    npm update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
       {
-        name: 'fiftechzone',
+        name: 'fiftech',
         exec_mode: 'cluster',
         instances: 'max', // Or a number of instances
         script: './node_modules/nuxt/bin/nuxt.js',
